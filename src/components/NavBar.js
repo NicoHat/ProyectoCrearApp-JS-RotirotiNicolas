935e239c10350a2f34c8be6e3ad655c430c9400b
NicoHat <nicolasrotiroti@hotmail.com>
2022-07-23 15:40:10 -0300

    Navbar y bootrstrap agregados

---
+++ src/components/NavBar.js
@@ -0,0 +1,18 @@
+export default function NavBar(){
+    return(
+        <ul class="nav justify-content-center">
+  <li class="nav-item">
+    <a class="nav-link" href="#">Inicio</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Indumentaria masculina</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Indumentaria femenina</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Calzados unisex</a>
+  </li>
+</ul>
+    )
+}
