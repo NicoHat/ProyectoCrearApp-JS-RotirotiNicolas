94a35eb8284e55dd94d04c02dd7541d645ea45c0
NicoHat <nicolasrotiroti@hotmail.com>
2022-07-29 16:49:25 -0300

    CartWidget e ItemListContainer agregados al proyecto

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 import Nav from "react-bootstrap/Nav";
 import NavBar from './components/NavBar';
+import ItemListContainer from './components/ItemListContainer';
+
 
 function App() {
   return (
     <><NavBar />
-    <br/>
-    <h1 className="text-center">Proximamente e-commerce "Dogdog"</h1>
+    <ItemListContainer greeting="proximamente e-commerce" />
     </>
   )
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,18 +1,27 @@
+import Cart from "./CartWidget.js"
 export default function NavBar(){
     return(
-        <ul class="nav justify-content-center">
-  <li class="nav-item">
-    <a class="nav-link" href="#">Inicio</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link" href="#">Indumentaria masculina</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link" href="#">Indumentaria femenina</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link" href="#">Calzados unisex</a>
-  </li>
-</ul>
+          <>
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <a class="navbar-brand" href="#">E-commerce Dogdog</a>
+            <div class="collapse navbar-collapse" id="navbarNav">
+              <ul class="navbar-nav">
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Inicio</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Indumentaria masculina</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Indumentaria femenina</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Calzados unisex</a>
+                </li>
+              </ul>
+            </div>
+            <Cart />
+          </nav>
+          </>
     )
 }
